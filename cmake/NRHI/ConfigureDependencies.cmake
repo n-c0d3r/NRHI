4255b36ea62cbb51f068a5348c167a71f4a4bc5c
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-05-12 03:26:51 +0700

    spirv-cross worked

--- cmake/NRHI/ConfigureDependencies.cmake
+++ cmake/NRHI/ConfigureDependencies.cmake
@@ -28,4 +28,13 @@ endif()
 
 
 
+#####################################################################################
+#   Add SPIRV-Cross subdirectory
+#####################################################################################
+if(NOT TARGET spirv-cross)
+    add_subdirectory("${NRHI_SUBMODULES_DIR}/SPIRV-Cross" "${NRHI_BINARY_DIR}/submodules/SPIRV-Cross")
+endif()
+
+
+
 message(STATUS "<NRHI::ConfigureDependencies> Configure dependencies done")
