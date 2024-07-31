5f57e7272fd6e6b944426d4ef9d89013107eeb01
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-08-01 03:07:10 +0700

    rename ImplementResourceBindFlag.cmake (dx11, dx12) to ImplementResourceFlag.cmake

--- cmake/NRHI/Drivers/DirectX11/ImplementEnums.cmake
+++ cmake/NRHI/Drivers/DirectX11/ImplementEnums.cmake
@@ -3,7 +3,7 @@ message(STATUS "<NRHI::Drivers::DirectX11> Start implementing enums")
 
 include(NRHI/Drivers/DXGI/FormatHelper)
 include(NRHI/Drivers/DirectX11/ImplementCommandListType)
-include(NRHI/Drivers/DirectX11/ImplementResourceBindFlag)
+include(NRHI/Drivers/DirectX11/ImplementResourceFlag)
 include(NRHI/Drivers/DirectX11/ImplementResourceType)
 include(NRHI/Drivers/DirectX11/ImplementResourceHeapType)
 include(NRHI/Drivers/DirectX11/ImplementResourceViewType)
