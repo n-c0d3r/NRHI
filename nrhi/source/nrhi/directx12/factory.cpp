30d8c154fa63d621bd21a1d8e867e53cd84c0229
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-08-21 06:35:19 +0700

    update dx12 debug layer

--- nrhi/source/nrhi/directx12/factory.cpp
+++ nrhi/source/nrhi/directx12/factory.cpp
@@ -13,21 +13,6 @@ namespace nrhi {
 
 		UINT dxgiFactoryFlags = 0;
 
-#if defined(_DEBUG)
-		// Enable the debug layer (requires the Graphics Tools "optional feature").
-		// NOTE: Enabling the debug layer after device creation will invalidate the active device.
-		{
-			ID3D12Debug* debugController = 0;
-			if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debugController))))
-			{
-				debugController->EnableDebugLayer();
-
-				// Enable additional debug layers.
-				dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
-			}
-		}
-#endif
-
         HRESULT hr = CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&factory_p_));
         NCPP_ASSERT(!FAILED(hr)) << "can't create factory";
 
