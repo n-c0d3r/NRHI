c3eddfac26ad41722fb877a8557bf72ad7b17eca
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-08-21 23:38:27 +0700

    Update resource_state_helper.cpp

--- nrhi/source/nrhi/directx12/resource_state_helper.cpp
+++ nrhi/source/nrhi/directx12/resource_state_helper.cpp
@@ -6,37 +6,20 @@ namespace nrhi
 {
 	b8 HD_directx12_resource_state::is_writable(ED_resource_state resource_state)
 	{
-		NRHI_ENUM_SWITCH(
-			resource_state,
-			NRHI_ENUM_CASE(
-				ED_resource_state::RENDER_TARGET,
-				return true;
-			)
-			NRHI_ENUM_CASE(
-				ED_resource_state::UNORDERED_ACCESS,
-				return true;
-			)
-			NRHI_ENUM_CASE(
-				ED_resource_state::DEPTH_WRITE,
-				return true;
-			)
-			NRHI_ENUM_CASE(
-				ED_resource_state::COPY_DEST,
-				return true;
-			)
-			NRHI_ENUM_CASE(
-				ED_resource_state::VIDEO_DECODE_WRITE,
-				return true;
-			)
-			NRHI_ENUM_CASE(
-				ED_resource_state::VIDEO_PROCESS_WRITE,
-				return true;
-			)
-			NRHI_ENUM_CASE(
-				ED_resource_state::VIDEO_ENCODE_WRITE,
-				return true;
-			)
-		);
+		if(flag_is_has(resource_state, ED_resource_state::RENDER_TARGET))
+			return true;
+		if(flag_is_has(resource_state, ED_resource_state::UNORDERED_ACCESS))
+			return true;
+		if(flag_is_has(resource_state, ED_resource_state::DEPTH_WRITE))
+			return true;
+		if(flag_is_has(resource_state, ED_resource_state::COPY_DEST))
+			return true;
+		if(flag_is_has(resource_state, ED_resource_state::VIDEO_DECODE_WRITE))
+			return true;
+		if(flag_is_has(resource_state, ED_resource_state::VIDEO_PROCESS_WRITE))
+			return true;
+		if(flag_is_has(resource_state, ED_resource_state::VIDEO_ENCODE_WRITE))
+			return true;
 
 		return false;
 	}
