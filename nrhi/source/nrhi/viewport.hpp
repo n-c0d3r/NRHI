9ad5411506db5266489a8e464b96996586881fef
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-08-25 17:55:48 +0700

    Update viewport.hpp

--- nrhi/source/nrhi/viewport.hpp
+++ nrhi/source/nrhi/viewport.hpp
@@ -39,18 +39,24 @@ namespace nrhi {
 
         union
         {
-            f32 min_x;
-            f32 min_y;
-            F_vector2_f32 min_xy = F_vector2_u32::zero();
+            struct
+            {
+                f32 min_x;
+                f32 min_y;
+            };
+            F_vector2_f32 min_xy = F_vector2_f32::zero();
         };
 
         f32 min_z = 0.0f;
 
         union
         {
-            f32 max_x;
-            f32 max_y;
-            F_vector2_f32 max_xy = F_vector2_u32::zero();
+            struct
+            {
+                f32 max_x;
+                f32 max_y;
+            };
+            F_vector2_f32 max_xy = F_vector2_f32::zero();
         };
 
         f32 max_z = 1.0f;
