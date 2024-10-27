bc0a126b0de8ff77c141aa59d218ba5cc5f92264
NCoder <68686710+n-c0d3r@users.noreply.github.com>
2024-10-28 06:50:59 +0700

    Update bootstrap.cpp

--- nrhi/samples/work_graphs/source/bootstrap.cpp
+++ nrhi/samples/work_graphs/source/bootstrap.cpp
@@ -45,6 +45,8 @@ int main() {
 
 	F_global_root_signature_subobject& global_root_signature_subobject = state_object_builder.add_global_root_signature_subobject();
 
+	F_local_root_signature_subobject& local_root_signature_subobject = state_object_builder.add_local_root_signature_subobject();
+
 	F_state_object_config_subobject& state_object_config_subobject = state_object_builder.add_state_object_config();
 	state_object_config_subobject.set_flags(ED_state_object_flag::ALLOW_STATE_OBJECT_ADDITIONS);
 
